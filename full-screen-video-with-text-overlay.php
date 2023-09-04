a8475dc05931bb8dd9c2e33f9958d8ae9accccc9
volkan <16579273+mevolkan@users.noreply.github.com>
2023-09-04 08:12:34 +0300

    no change

--- full-screen-video-with-text-overlay.php
+++ full-screen-video-with-text-overlay.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       Full screen video with text overlay
- * Description:       Full screen video with text overlay
+ * Description:       This is an example of a full screen video being shown as a background at any screen size. This is accomplished with giving video absolute positioning, full viewport width and height, and using 'object-fit: cover;' 
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
